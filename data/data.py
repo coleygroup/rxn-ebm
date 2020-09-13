70c19fb3c2a4341f00c4c06cbc73fd0ceff714f3
Lin Min Htoo <linmin001@e.ntu.edu.sg>
2020-09-14 01:27:54 +0800

    added resumeEBM.py + experiment.utils

--- data/data.py
+++ data/data.py
@@ -11,6 +11,9 @@ from rdkit.Chem import rdChemReactions
 from rdkit import DataStructs
 import numpy as np
 
+import nmslib
+
+
 def create_rxn_MorganFP_fromFP(raw_fp, num_rcts, fp_type='diff', 
                                rctfp_size=4096, prodfp_size=4096, dtype='int8'):
     '''
@@ -94,33 +97,47 @@ class ReactionDataset(Dataset):
                 'num_bits': # bits to randomly corrupt in rxn fingerprint 
         ''' 
         self.trainargs = trainargs
-        if mode == 'bit_corruption':
-            self.rxn_fp = sparse.load_npz(base_path + '_' + key + '.npz')
-            self.num_neg = trainargs['num_neg']
-            self.num_bits = trainargs['num_bits']
-        else:
-            self.fp_raw_num_rcts = sparse.load_npz(base_path + '_' + key + '.npz')        
-            if 'cluster_path' in trainargs.keys(): # doing cosine similarity search
-                with open(trainargs['cluster_path'], 'rb') as handle:
-                    self.clusterindex = pickle.load(handle)
-
-                assert 'sparseFP_vocab_path' in trainargs.keys(), 'You provided cluster_path but not sparseFP_vocab_path!!'
-                self.sparseFP_vocab = sparse.load_npz(trainargs['sparseFP_vocab_path'])
-
-            assert 'num_neg_prod' in trainargs.keys() and 'num_neg_rct' in trainargs.keys(), \
-            'You did not provide num_neg_prod and num_neg_rct!'
-            self.num_neg_prod = trainargs['num_neg_prod']
-            self.num_neg_rct = trainargs['num_neg_rct']
-        
         self.mode = mode
+        if self.mode == 'bit_corruption':
+#             print('bit_corruption')
+            self.rxn_fp = sparse.load_npz(base_path + '_' + key + '.npz')
+            self.num_neg = self.trainargs['num_neg']
+            assert self.num_neg > 0, 'num_neg must be positive!'
+            self.num_bits = self.trainargs['num_bits']
+            assert self.num_bits > 0, 'num_bits must be positive!'
+            
+        else:
+#             print('cosine/random sampling')            
+            self.fp_raw_num_rcts = sparse.load_npz(base_path + '_' + key + '.npz')            
+            if 'cluster_path' in self.trainargs.keys(): # doing cosine similarity search
+                self._init_searchindex()
+   
+            assert 'num_neg_prod' in self.trainargs.keys() and 'num_neg_rct' in self.trainargs.keys(), \
+            'You are not doing bit_corruption, but did not provide num_neg_prod and num_neg_rct!'
+            self.num_neg_prod = self.trainargs['num_neg_prod']
+            self.num_neg_rct = self.trainargs['num_neg_rct']
+            assert self.num_neg_prod > 0 and self.num_neg_rct > 0, 'num_neg cannot be negative!' 
         
-        self.fp_type = trainargs['fp_type']
-        self.fp_radius = trainargs['fp_radius'] # not needed if loading pre-computed fingerprints
-        self.rctfp_size = trainargs['rctfp_size']
-        self.prodfp_size = trainargs['prodfp_size']
-        assert trainargs['rctfp_size'] == trainargs['prodfp_size']
-        self.save_neg = save_neg
-        self.show_neg = show_neg
+        self.fp_type = self.trainargs['fp_type']
+        self.rctfp_size = self.trainargs['rctfp_size']
+        self.prodfp_size = self.trainargs['prodfp_size']
+        assert trainargs['rctfp_size'] == self.trainargs['prodfp_size'], \
+        'rctfp_size must be equal to prodfp_size!'
+#         self.save_neg = save_neg
+#         self.show_neg = show_neg
+
+    def _init_searchindex(self):
+        self.sparseFP_vocab = sparse.load_npz(self.trainargs['sparseFP_vocab_path'])
+        if self.mode == 'cosine_spaces':
+            self.clusterindex = nmslib.init(method='hnsw', space='cosinesimil_sparse', 
+                                data_type=nmslib.DataType.SPARSE_VECTOR)
+            self.clusterindex.loadIndex(self.trainargs['cluster_path'], load_data=True)
+            
+            if 'query_params' in self.trainargs.keys():
+                self.clusterindex.setQueryTimeParams(self.trainargs['query_params'])
+        else:
+            with open(self.trainargs['cluster_path'], 'rb') as handle:
+                self.clusterindex = pickle.load(handle)
 
     def random_sample_negative(self, raw_fp, num_rcts, mode):
         '''
@@ -139,7 +156,16 @@ class ReactionDataset(Dataset):
         else:
             raw_fp[-1, :] = new_raw_fp[-1, :] 
         return raw_fp 
-
+    
+    def hopefully_faster_vstack(self, tuple_of_arrays):
+        array_1, array_2 = tuple_of_arrays
+        width = array_1.shape[1]
+        length_1, length_2 = array_1.shape[0], array_2.shape[0]
+#         assert array_1.shape[1] == array_2.shape[1]
+        array_out = np.empty((length_1 + length_2, width))
+        array_out[:length_1] = array_1
+        array_out[length_1:] = array_2
+        
     def cosine_sample_negative(self, raw_fp, num_rcts):
         ''' 
         Replace product w/ approx nearest neighbor based on cosine similarity 
@@ -149,21 +175,35 @@ class ReactionDataset(Dataset):
         
         Returns:
             list of num_neg X dense nparrays of the same shape as raw_fp, with product replaced 
+            
+        TODO: might be faster to sample rct_idx's for entire batch (have to design custom collate_fn)
         '''
         rct_idx = random.choice(np.arange(num_rcts)) 
-        # might be faster to sample rct_idx's for entire batch (have to design custom collate_fn)
-        
         rct_prod_sparse = sparse.csr_matrix(raw_fp.copy()[[rct_idx, -1]], dtype='int8')
-        nn_rct_indices, nn_prod_indices = self.clusterindex.search(rct_prod_sparse, k=max(self.num_neg_prod, self.num_neg_rct)+1, 
-                                                   return_distance=False)  #[0][1:]
-        nn_prod_indices, nn_rct_indices = [int(idx) for idx in nn_prod_indices], [int(idx) for idx in nn_rct_indices]
-        nn_prod_FPs = [self.sparseFP_vocab[idx].toarray() for idx in nn_prod_indices[1: self.num_neg_prod + 1]]
-        nn_rct_FPs = [self.sparseFP_vocab[idx].toarray() for idx in nn_rct_indices[1: self.num_neg_rct + 1]] # 1 x 4096 nparray
         
-        if self.show_neg:
-            return [np.vstack((raw_fp[:-1], FP)) for FP in nn_prod_FPs] + [xform_rctFP(rctFP, rct_idx, raw_fp) for rctFP in nn_rct_FPs], nn_prod_indices, nn_rct_indices
+        if self.mode == 'cosine_spaces':
+            nn_rct_idx_dist, nn_prod_idx_dist = self.clusterindex.knnQueryBatch(rct_prod_sparse, 
+                                                                                k = max(self.num_neg_prod, self.num_neg_rct) + 1, 
+                                                                                num_threads = 4) # or self.trainargs['num_threads']
+            nn_rct_idxs, nn_prod_idxs = nn_rct_idx_dist[0], nn_prod_idx_dist[0]
         else:
-            return [np.vstack((raw_fp[:-1], FP)) for FP in nn_prod_FPs] + [xform_rctFP(rctFP, rct_idx, raw_fp) for rctFP in nn_rct_FPs]  
+            nn_rct_idxs, nn_prod_idxs = self.clusterindex.search(rct_prod_sparse, k=max(self.num_neg_prod, self.num_neg_rct)+1, 
+                                                   return_distance=False)  #[0][1:]
+            nn_rct_idxs, nn_prod_idxs = [int(idx) for idx in nn_rct_idxs], [int(idx) for idx in nn_prod_idxs]
+        nn_prod_FPs = [self.sparseFP_vocab[idx].toarray() for idx in nn_prod_idxs[1: self.num_neg_prod + 1]]
+        nn_rct_FPs = [self.sparseFP_vocab[idx].toarray() for idx in nn_rct_idxs[1: self.num_neg_rct + 1]] # 1 x 4096 nparray
+        
+        # try pre-allocating output list in memory, to avoid list concatenation (which might be slow)
+        out_FPs = [0] * (self.num_neg_prod + self.num_neg_rct)
+        for i, prod_FP in enumerate(nn_prod_FPs):
+            out_FPs[i] = np.vstack((raw_fp[:-1], prod_FP))
+        for j, rct_FP in enumerate(nn_rct_FPs):
+            out_FPs[self.num_neg_prod + j] = xform_rctFP(rct_FP, rct_idx, raw_fp)
+        return out_FPs
+#         if self.show_neg:
+#             return [np.vstack((raw_fp[:-1], FP)) for FP in nn_prod_FPs] + [xform_rctFP(rctFP, rct_idx, raw_fp) for rctFP in nn_rct_FPs], nn_prod_indices, nn_rct_indices
+#         else:
+#             return [np.vstack((raw_fp[:-1], FP)) for FP in nn_prod_FPs] + [xform_rctFP(rctFP, rct_idx, raw_fp) for rctFP in nn_rct_FPs]  
             
     def random_bit_corrupter(self, rxn_fp, num_bits=10):
         ''' Randomly selects <num_bits> bits in rxn_fp & randomly replaces them w/ -1, 0 or 1
@@ -173,7 +213,6 @@ class ReactionDataset(Dataset):
         Returns:
             corrupted fp of same shape and type as rxn_fp
         '''
-        assert num_bits > 0, 'num_bits must be positive!'
         rdm_idx = random.sample(range(rxn_fp.shape[-1]), k=num_bits)
         rxn_fp[0, rdm_idx] = [random.choice([-1, 0, 1]) for bit in rxn_fp[0, rdm_idx]]
         return rxn_fp
@@ -199,26 +238,30 @@ class ReactionDataset(Dataset):
             num_rcts = num_rcts[0][0]
             pos_rxn_fp = create_rxn_MorganFP_fromFP(pos_raw_fp.copy(), num_rcts, fp_type=self.fp_type, 
                                                     rctfp_size=self.rctfp_size, prodfp_size=self.prodfp_size)
-
-            assert self.num_neg_prod > 0 and self.num_neg_rct > 0, 'num_neg cannot be negative!'   
+            
             if 'cluster_path' in self.trainargs.keys(): # cosine similarity search
-                if self.show_neg:
-                    neg_raw_fps, nn_prod_indices, nn_rct_indices = self.cosine_sample_negative(pos_raw_fp, num_rcts)
-                else:
-                    neg_raw_fps = self.cosine_sample_negative(pos_raw_fp, num_rcts)
+#                 if self.show_neg:
+#                     neg_raw_fps, nn_prod_indices, nn_rct_indices = self.cosine_sample_negative(pos_raw_fp, num_rcts)
+#                 else:
+                neg_raw_fps = self.cosine_sample_negative(pos_raw_fp, num_rcts)
             else:
-                neg_raw_fps = [self.random_sample_negative(pos_raw_fp.copy(), num_rcts, 'prod') 
-                               for i in range(self.num_neg_prod)] + [
-                               self.random_sample_negative(pos_raw_fp.copy(), num_rcts, 'rct') 
-                               for i in range(self.num_neg_rct)]
+                neg_raw_fps = [0] * (self.num_neg_prod + self.num_neg_rct)
+                for i in range(self.num_neg_prod):
+                    neg_raw_fps[i] = self.random_sample_negative(pos_raw_fp.copy(), num_rcts, 'prod')
+                for j in range(self.num_neg_rct):
+                    neg_raw_fps[self.num_neg_prod + j] = self.random_sample_negative(pos_raw_fp.copy(), num_rcts, 'rct') 
+#                 neg_raw_fps = [self.random_sample_negative(pos_raw_fp.copy(), num_rcts, 'prod') 
+#                                for i in range(self.num_neg_prod)] + [
+#                                self.random_sample_negative(pos_raw_fp.copy(), num_rcts, 'rct') 
+#                                for i in range(self.num_neg_rct)]
 
             neg_rxn_fps = [create_rxn_MorganFP_fromFP(neg_raw_fp.copy(), num_rcts, fp_type=self.fp_type, 
                                                       rctfp_size=self.rctfp_size, prodfp_size=self.prodfp_size)
                             for neg_raw_fp in neg_raw_fps]
-            if self.show_neg:
-                return torch.Tensor([pos_rxn_fp, *neg_rxn_fps]), nn_prod_indices, nn_rct_indices
-            else:
-                return torch.Tensor([pos_rxn_fp, *neg_rxn_fps])
+#             if self.show_neg:
+#                 return torch.Tensor([pos_rxn_fp, *neg_rxn_fps]), nn_prod_indices, nn_rct_indices
+#             else:
+            return torch.Tensor([pos_rxn_fp, *neg_rxn_fps])
 
     def __len__(self):
         if self.mode == 'bit_corruption':
--- resumeEBM.py
+++ resumeEBM.py
@@ -1,29 +1,39 @@
-def trainEBM():
+def resumeEBM():
     import os
     import numpy as np
     from datetime import date
     import torch
 
-    from experiment import Experiment
+    from experiment.experiment import Experiment
+    from experiment.utils import load_model_opt_and_stats, setup_paths
     from model.FF import FeedforwardEBM
-    from model.utils import setup_paths
-    from data.data import ReactionDataset
-    from torch.utils.data import DataLoader
 
     LOCAL = True # CHANGE THIS 
     cosine = False # CHANGE THIS 
     multi = False # monocluster or multicluster
     mode = 'bit_corruption' # CHANGE THIS - bit_corruption or random_sampling or cosine
+    model = 'Feedforward'
     expt_name = 'expt3'
 
-    checkpoint_folder, base_path, cluster_path, sparseFP_vocab_path = setup_paths(expt_name, mode, LOCAL, cosine, multi) 
+    load_pretrained = True
+    date_trained = '13_09_2020'
+    stats_filename = 'Feedforward_expt3_stats.pkl'  
+    opt = 'Adam' 
+
+    checkpoint_folder, base_path, cluster_path, sparseFP_vocab_path = setup_paths(expt_name, mode,
+                                                                                   load_pretrained, date_trained, 
+                                                                                   LOCAL, cosine, multi)
+
+    model, optimizer, stats = load_model_opt_and_stats(stats_filename, base_path, cluster_path,
+                                                  sparseFP_vocab_path, checkpoint_folder,
+                                                  model, cosine, opt)
 
     trainargs = {
-    'model': 'Feedforward',
+    'model': model,
     'hidden_sizes': [1024, 256],  
     'output_size': 1,
     'dropout': 0.1,  
-    
+
     'batch_size': 64,
     'activation': 'ReLU',  
     'optimizer': torch.optim.Adam,
@@ -37,29 +47,30 @@ def trainEBM():
     'checkpoint': True,
     'model_seed': 1337,
     'random_seed': 0, # affects neg rxn sampling since it is random
-    
+
     'rctfp_size': 4096, # if fp_type == 'diff', ensure that both rctfp_size & prodfp_size are identical!
     'prodfp_size': 4096,
     'fp_radius': 3,
     'fp_type': 'diff',
-    
+
     'num_neg_prod': 5, 
     'num_neg_rct': 5,
     'num_bits': 3, 
     'num_neg': 10,
-    
+
     'base_path': base_path, # refer to top of notebook 
     'checkpoint_path': checkpoint_folder,
     'cluster_path': cluster_path,
     'sparseFP_vocab_path': sparseFP_vocab_path,
     'query_params': {'efSearch': 100}, # good value to hit 96% recall
- 
+
     'expt_name': expt_name,
     'device': torch.device("cuda" if torch.cuda.is_available() else "cpu")
     }   
 
-    model = FeedforwardEBM(trainargs)
-    experiment = Experiment(model, trainargs, mode=mode)
+    experiment = Experiment(model, stats['trainargs'], mode=mode, 
+                            load_optimizer=optimizer, load_checkpoint=True, load_stats=stats, 
+                            stats_filename=stats_filename, begin_epoch=stats['best_epoch'] + 1)
 
     experiment.train()
     experiment.test()
@@ -68,5 +79,5 @@ def trainEBM():
     scores = experiment.get_topk_acc(key='train', k=1, repeats=1, name_scores='scores_{}_{}_{}'.format(mode, 'train', expt_name))
 
 if __name__ == '__main__': 
-    trainEBM()
+    resumeEBM()
     # TO DO: parse arguments from terminal
