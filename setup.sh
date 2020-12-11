bdc4a04eb1d11fd720ac71b880d1cfb7fe0d6c2c
nos <siriuself1103@gmail.com>
2020-12-11 09:19:24 -0500

    comment out PTG installation. Claiming write token over master branch

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 TORCH_VER=1.6.0
 CUDA_VER=cu102
 
-conda create -n rxnebm python=3.6 tqdm pathlib typing scipy pandas joblib -y
+conda create -n rxnebm python=3.6 tensorflow-gpu=1.14 tqdm pathlib typing scipy pandas joblib -y
 conda activate rxnebm
 
-conda install -y pytorch=${TORCH_VER} torchvision cudatoolkit=10.2 torchtext -c pytorch
+conda install -y pytorch=${TORCH_VER} torchvision cudatoolkit=10.1 torchtext -c pytorch
 conda install -y -c conda-forge rdkit
 
 pip install --no-binary :all: nmslib
-pip install crem gdown
+pip install crem gdown gast==0.2.2
 
 # install rdchiral (needed by retrosim)
 pip install -e "git://github.com/connorcoley/rdchiral.git#egg=rdchiral"
 
 # install torch geometric
-pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
-pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
-pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
-pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
-pip install torch-geometric
+# pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
+# pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
+# pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
+# pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
+# pip install torch-geometric
 
 # install gln as a package
 cd ./rxnebm/proposer/GLN_original
@@ -26,4 +26,4 @@ pip install -e .
 cd ../../..
 
 # install tensorflow
-pip install tensorflow-gpu==1.14 gast==0.2.2
+# pip install tensorflow-gpu==1.14 gast==0.2.2
