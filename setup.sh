535067675cb2b0dc6c01cfe4bb92a19d2b25e902
linmin001 <linmin001@e.ntu.edu.sg>
2021-01-03 14:19:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 TORCH_VER=1.6.0
-CUDA_VER=cu101
+CUDA_VER=10.1
+CUDA_CODE=cu101
 
-conda create -n rxnebm python=3.6 tensorflow-gpu=1.14 tqdm pathlib typing scipy pandas joblib -y
-conda activate rxnebm
+conda create -n rxnebm_ztu python=3.6 tensorflow-gpu=1.14 tqdm pathlib typing scipy pandas joblib -y
+conda activate rxnebm_ztu
 
-conda install -y pytorch=${TORCH_VER} torchvision cudatoolkit=10.1 torchtext -c pytorch
-conda install -y -c conda-forge rdkit
+conda install -y pytorch=${TORCH_VER} torchvision cudatoolkit=${CUDA_VER} torchtext -c pytorch
+conda install -y rdkit -c rdkit
 
 pip install --no-binary :all: nmslib
 pip install crem gdown OpenNMT-py==1.2.0 networkx==2.5 gast==0.2.2
@@ -14,16 +15,13 @@ pip install crem gdown OpenNMT-py==1.2.0 networkx==2.5 gast==0.2.2
 pip install -e "git://github.com/connorcoley/rdchiral.git#egg=rdchiral"
 
 # install torch geometric
-# pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
-# pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
-# pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
-# pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+{CUDA_VER}.html
-# pip install torch-geometric
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+${CUDA_CODE}.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+${CUDA_CODE}.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+${CUDA_CODE}.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-${TORCH_VER}+${CUDA_CODE}.html
+pip install torch-geometric
 
-# install gln as a package
+# install gln as a package, must install on a machine with CUDA to enable CUDA GPU ops
 # cd ./rxnebm/proposer/GLN_original
 # pip install -e .
 # cd ../../..
-
-# install tensorflow
-# pip install tensorflow-gpu==1.14 gast==0.2.2
